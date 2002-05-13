06968ee38dd8855c7e31c57d6c1aec4789b98f2b
Mark Crichton <crichton@mono-cvs.ximian.com>
2002-05-13 18:53:49 +0000

    Reverting change.
    svn path=/trunk/csvorbis/; revision=4600

--- csvorbis/Lsp.cs
+++ csvorbis/Lsp.cs
@@ -42,6 +42,12 @@ namespace csvorbis
 	class Lsp
 	{
 
+		[StructLayout(LayoutKind.Explicit, Size=32, CharSet=CharSet.Ansi)]
+		struct FloatHack 
+		{
+			[FieldOffset(0)] public float fh_float;
+			[FieldOffset(0)] public int fh_int;
+		}
 		static float M_PI=(float)(3.1415926539);
 
 		internal static void lsp_to_curve(float[] curve,
@@ -51,43 +57,76 @@ namespace csvorbis
 		{
 			int i;
 			float wdel=M_PI/ln;
-
-			for(i=0;i<m;i++)
-				lsp[i]=2.0f * (float)Math.Cos(lsp[i]);
+			for(i=0;i<m;i++)lsp[i]=Lookup.coslook(lsp[i]);
+			int m2=(m/2)*2;
 
 			i=0;
 			while(i<n)
 			{
-				int j,k=map[i];
-				float p=0.5f;
-				float q=0.5f;
-				float w=2.0f * (float)Math.Cos(wdel*k);
+				FloatHack fh = new FloatHack();
+				int k=map[i];
+				float p=.7071067812f;
+				float q=.7071067812f;
+				float w=Lookup.coslook(wdel*k);
+				//int ftmp=0;
+				int c=(int)((uint)m >> 1);
 
-				for(j=1; j < m; j += 2)
+				for(int j=0;j<m2;j+=2)
 				{
-					q *= w - lsp[j-1];
-					p *= w - lsp[j];
+					q*=lsp[j]-w;
+					p*=lsp[j+1]-w;
 				}
 
-				if(j == m)
+				if((m&1)!=0)
 				{
 					/* odd order filter; slightly assymetric */
 					/* the last coefficient */
-					q*=w-lsp[j-1];
-					p*=p*(4.0f-w*w);
+					q*=lsp[m-1]-w;
 					q*=q;
+					p*=p*(1.0f-w*w);
 				}
 				else
 				{
 					/* even order filter; still symmetric */
-					q*=q*(2.0f+w);
-					p*=p*(2.0f-w);
+					q*=q*(1.0f+w);
+					p*=p*(1.0f-w);
 				}
 
-				q = (float)(Math.Exp(amp/Math.Sqrt(p+q)-ampoffset))
-					    * 0.11512925f;
-				curve[i] *= q;
-				while(map[++i] == k) curve[i] *= q;
+				//  q=frexp(p+q,&qexp);
+				q=p+q;
+				fh.fh_float = q;
+				int hx=fh.fh_int;
+				int ix=0x7fffffff&hx;
+				int qexp=0;
+
+				if(ix>=0x7f800000||(ix==0))
+				{
+					// 0,inf,nan
+				}
+				else
+				{
+					if(ix<0x00800000)
+					{            // subnormal
+						q*=3.3554432000e+07F;        // 0x4c000000
+						fh.fh_float = q;
+						hx=fh.fh_int;
+						ix=0x7fffffff&hx;
+						qexp=-25;
+					}
+					qexp += (int)(((uint)ix >> 23)-126);
+					hx=(int)((hx&0x807fffff)|0x3f000000);
+					fh.fh_int = hx;
+					q=fh.fh_float;
+				}
+
+				q=Lookup.fromdBlook(amp*
+					Lookup.invsqlook(q)*
+					Lookup.invsq2explook(qexp+m)-ampoffset);
+
+				do{curve[i++]*=q;}
+					//    do{curve[i++]=q;}
+				while(i<n&&map[i]==k);
+
 			}
 		}
 	}
